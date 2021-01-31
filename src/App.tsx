04c5be8afba65d4c9dd09f882fa126cff885ebb6
christopher <christoph.roa@gmail.com>
2021-01-31 01:48:07 -0500

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import AddInsure from './views/AddInsure/AddInsure';
 import ChosePlan from './views/ChosePlan/ChosePlan';
 import Login from './views/Login/Login';
 import Success from './views/Success/Success';
+import { Router,RouteComponentProps } from "@reach/router";
 
-function App():JSX.Element {
-
-
-  return (
-  // <Login/>
-  // <Success/>
-  // <AddInsure/>
-  <ChosePlan/>
-  );
-}
-
+const App = () => (
+  <Router>
+    <RouterPage path="/" pageComponent={<Login />} />
+    <RouterPage path="/insure" pageComponent={<AddInsure />} />
+    <RouterPage path="/plan" pageComponent={<ChosePlan />} />
+    <RouterPage path="/success" pageComponent={<Success />} />
+  </Router>
+);
 export default App;
+
+const RouterPage = (
+  props: { pageComponent: JSX.Element } & RouteComponentProps
+) => props.pageComponent;
+
